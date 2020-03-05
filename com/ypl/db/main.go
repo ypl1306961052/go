356759f7b91e4cd9e902ee21b416b07fa46803c1
yangpeilin <1306961052@qq.com>
2020-03-05 21:41:01 +0800

    go mysql的学习

--- com/ypl/db/main.go
+++ com/ypl/db/main.go
@@ -1,7 +1,156 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"strings"
+	"sync"
+)
+
+type Person struct {
+	age   int
+	name  string
+	adder string
+}
 
 func main() {
-	sql.Open("mysql",)
+	initDB()
+	defer closeDb()
+	//goDb()
+	//selectColumns()
+	Transaction()
+}
+func f1() {
+	fmt.Println("测试")
+}
+func goDb() {
+
+	selectDb()
+}
+
+var db *sql.DB
+var once sync.Once
+
+func initDB() {
+
+	once.Do(func() {
+		fmt.Println("开启数据库链接.....")
+		var err error
+		db, err = sql.Open("mysql", "root:ypl123456@tcp(127.0.0.1:3306)/go?charset=utf8")
+		if err != nil {
+			fmt.Println("数据链接错误", err)
+			panic("数据链接错误")
+		}
+		err = db.Ping()
+		if err != nil {
+			fmt.Println("数据链接失败,请检查网络")
+			print("数据链接失败,请检查网络")
+		}
+		db.SetMaxOpenConns(20)
+		db.SetMaxIdleConns(10)
+		db.SetMaxOpenConns(100)
+		fmt.Println("数据库初始化完成")
+	})
+}
+func closeDb() {
+	if db != nil {
+		err := db.Close()
+		if err != nil {
+
+			fmt.Println("关闭数据数据库失败", err)
+		}
+	}
+	fmt.Println("关闭数据库链接")
+}
+func selectDb() {
+
+	rows, err := db.Query("select * from Person where age>?;", 23)
+	defer rows.Close()
+	if err != nil {
+		fmt.Println("查询数据失败")
+	}
+	if rows == nil {
+		fmt.Println("获取数据错误")
+		return
+	}
+	peoples := make([]Person, 0)
+	for rows.Next() {
+		var p = Person{}
+		err := rows.Scan(&p.age, &p.name, &p.adder)
+		if err != nil {
+			fmt.Println("获取数据错误")
+			continue
+		}
+		peoples = append(peoples, p)
+
+	}
+	fmt.Println(peoples)
+
+}
+func selectOne(age int) {
+
+	row := db.QueryRow("select age,name,adder from Person where age=?", age)
+	var p = &Person{}
+	err := row.Scan(&p.age, &p.name, &p.adder)
+	if err != nil {
+		fmt.Println("查询失败", err)
+	}
+	//fmt.Println(p)
+
+}
+func insetDb() {
+
+	rest, err := db.Exec("insert into Person(age,name,adder) values (?,?,?)", 1, "杨沛霖", "上海")
+	checkErr("", err)
+	//id, err := rest.LastInsertId()
+	//checkErr("error id,", &err)
+	//fmt.Println(id)
+	count, _ := rest.RowsAffected()
+	fmt.Println("影响行数:", count)
+}
+func checkErr(lineHead string, err error) {
+	if err != nil {
+		fmt.Println(lineHead, err)
+	}
+}
+func selectColumns() {
+	stmt, err := db.Prepare("select * from Person") //[id uname age mobile]
+	checkErr("", err)
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	defer rows.Close()
+	checkErr("", err)
+	clou, err := rows.Columns()
+	clouType, err := rows.ColumnTypes()
+	checkErr("", err)
+	for _, item := range clouType {
+		fmt.Println(item.Name())
+		fmt.Println(item.DatabaseTypeName())
+		fmt.Println(item.DecimalSize())
+		fmt.Println(item.Length())
+		fmt.Println(item.Nullable())
+	}
+	checkErr("", err)
+	fmt.Println(strings.Join(clou, ","))
+
+}
+func Transaction() {
+	t, err := db.Begin()
+	checkErr("", err)
+	_, err = t.Exec("update  Person set age=age+10 where name='马飞飞'")
+	if err != nil {
+		fmt.Println("出现错误")
+		t.Rollback()
+		fmt.Println("事务:1回滚")
+		return
+	}
+	_, err = t.Exec("update  Person set age=age+10 where name='杨kk'")
+	if err != nil {
+		fmt.Println("出现错误")
+		t.Rollback()
+		fmt.Println("事务:2回滚")
+		return
+	}
+	t.Commit()
 }
---
+++ com/ypl/db/main_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGoDb(t *testing.T) {
+	goDb()
+	t.Log("测试成功")
+}
+func TestF1(t *testing.T) {
+	f1()
+
+}
+func TestSelectOne(t *testing.T) {
+	selectOne(34)
+
+}
+
+//基准测试
+func BenchmarkSelectOne(b *testing.B) {
+	//initDB()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+
+		selectOne(34)
+	}
+	b.Log("测试结束")
+
+}
+
+//并行测试
+func BenchmarkSelectOneParallel(b *testing.B) {
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			selectOne(34)
+		}
+	})
+}
+//1132           1113969 ns/op             497 B/op         13 allocs/op
+func BenchmarkInsetDb(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		insetDb()
+	}
+	b.Log("测试插入数据结束")
+}
+//4087            727315 ns/op             499 B/op         13 allocs/op
+func BenchmarkInsetDbParallel(b *testing.B) {
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			insetDb()
+		}
+	})
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("测试开始")
+	initDB()
+	fmt.Println("初始化数据库链接")
+	m.Run()
+	fmt.Println("测试收尾工作开始")
+	clearData()
+	closeDb()
+
+}
+
+func clearData() {
+	rest, err := db.Exec("delete from Person where age=1")
+	checkErr("清楚数据错误", err)
+	count, err := rest.RowsAffected()
+	checkErr("", err)
+	println(count)
+
+}
